e13caa87ad9e3f22a2c23d17cdc4812d7313b694
Tommy Fannon <tfannon@gmail.com>
2017-01-06 18:23:43 -0500

    stuff

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using EFCoreSqlServer;
 
 namespace ConsoleApplication
 {
@@ -6,7 +8,28 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var db = new BloggingContext())
+            {
+                Console.WriteLine(Environment.GetEnvironmentVariable("DBServer"));
+                if (!db.Blogs.Any())
+                {
+                    Console.WriteLine("Creating some blog entries");
+                    for (var i = 0; i < 10; i++)
+                    {
+                        var semiRandomName = Guid.NewGuid().ToString().Substring(0, 8);
+                        var blog = new Blog {Url = $"http://{semiRandomName}.com"};
+                        db.Blogs.Add(blog);
+                    }
+                    db.SaveChanges();
+                }
+                Console.WriteLine("Reading blog entries");
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine($"{blog.BlogId} : {blog.Url}");
+                }
+                Console.WriteLine("Press return");
+                Console.ReadLine();
+            }            
         }
     }
 }
